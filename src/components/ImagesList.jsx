db31721452afe1231d64775ed32ddd8fbf60cb29
durdona <durdona>
2025-07-18 23:38:51 +0500

    reached

--- src/components/ImagesList.jsx
+++ src/components/ImagesList.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-function ImagesList({image}) {
+function ImagesList({ image }) {
+  console.log(image);
   return (
-    <li >
-      <img
-        className="photo rounded-2xl"
-        src={image.urls.regular}
-        alt=""
-      />
+    <li>
+      <div className="regular relative">
+        <img className="photo rounded-2xl " src={image.urls.regular} alt="" />
+        <div className="absolute bottom-3 left-2 flex items-center gap-1">
+          <img
+            className="rounded-4xl "
+            src={image.user.profile_image.small}
+            alt=""
+          />
+          <div className="">
+              <p className="line-clamp-1">@{image.user.username}</p>
+              <span className="opacity-50 line-clamp-1">{image.user.location}</span>
+          </div>
+        </div>
+      </div>
     </li>
   );
 }
